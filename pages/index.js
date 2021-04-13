684405617078b747f88a66e9d0b8befb9d939980
Richard Gilbert <gilbert67@gmail.com>
2021-04-12 21:23:11 -0700

    Another title change.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to VIP!
+          Welcome to Vation Innovation Platform!
         </h1>
 
         <p className={styles.description}>
