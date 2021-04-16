efb2aea79c54edda9ceedd8c03edd5f893f33baa
Richard Gilbert <gilbert67@gmail.com>
2021-04-16 09:59:27 -0700

    Updated "companies" page with data from two public apis.

--- pages/companies.js
+++ pages/companies.js
@@ -1,12 +1,64 @@
-const Companies = () => {
+import React, { useEffect, useState } from 'react'
+
+const StarWarsCharsApi = `https://swapi.dev/api/people/`
+const BeatlesAlbumsApi = `https://the-beatles-api.herokuapp.com/api/v1/albums/`
+
+const Companies = ({data}) => {
+
+  const [StarWarsChars, SetStarWarsChars] = useState([]);
+  const [BeatlesAlbums, SetBeatlesAlbums] = useState([]);
+
+  // Dynamically pull my page title.
+  // https://stackoverflow.com/questions/62781907/react-hooks-0-vs-empty-array-as-second-argument-in-useeffect
+  useEffect( () => {
+
+    async function GetStarWarsChars() {
+      const StarWarsCharsData = await fetch(StarWarsCharsApi)
+      const data = await StarWarsCharsData.json()
+      // console.log(data.results)
+      SetStarWarsChars(data.results)
+    }
+    GetStarWarsChars()
+
+    async function GetBeatlesAlbums() {
+      const BeatlesAlbumsData = await fetch(BeatlesAlbumsApi)
+      const data = await BeatlesAlbumsData.json()
+      // console.log(data)
+      SetBeatlesAlbums(data)
+    }
+    GetBeatlesAlbums()
+
+  }, [])
+
+
   return (
-    <div>
+    <>
       <h1>Companies</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-    </div>
-  );
+
+      <div id={"dynamic-data-wrapper"} className={"fontsize__small cols cols__equal"}>
+        <div className={"col col__first"}>
+          <h3>Star Wars characters, dynamic data from an API:</h3>
+          <ul>
+            {StarWarsChars.length === 0 ? <li>Loading...</li> : ''}
+            {StarWarsChars.map(character => (
+              <li key={character.name}>
+                {character.name} ({character.height}cm)
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={"col col__last"}>
+          <h3>Beatles albums, dynamic data from an API:</h3>
+          <ul>
+            {BeatlesAlbums.length === 0 ? <li>Loading...</li> : ''}
+            {BeatlesAlbums.map(album => (
+              <li key={album.albumName}>{album.albumName}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Companies;
